4c7fa287b7ad7d8ba77d291b7952173803e91d5b
Fernando <fernandocgoveia@gmail.com>
2026-02-24 12:51:50 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,3 +1,9 @@
+import model.entities.Department;
+import model.entities.Seller;
+
 void main(){
-    
+    Department department = new Department(1,"computer");
+    Seller seller = new Seller(2,"Jhon", "jhon@gmail.com",new Date(),5000.0,department);
+
+    IO.println(seller);
 }
